aa93191d6e2949af0c4e0c33942f8e38621ab72f
Nathaniel McGregor <nathanielmcgr@gmail.com>
2025-06-11 17:11:42 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("app-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html", "/styles.css", "/script.js"]);
+      return cache.addAll(["/", '/index.html', '/styles.css', '/script.js', '/icons/awp-logo-192x192.png', '/icons/awp-logo-512x512.png']);
     })
   );
 });
