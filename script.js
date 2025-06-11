dab5af6b9ee73a9ea4c555edbc1dabdb8eb1da60
Nathaniel McGregor <nathanielmcgr@gmail.com>
2025-06-11 15:11:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -140,13 +140,13 @@ window.addEventListener("load", hideLoader);
 
 
 
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/service-worker.js')
-//       .then(reg => console.log("Service Worker Registered"))
-//       .catch(err => console.log("Service Worker Failed", err));
-//   });
-// }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log("Service Worker Registered"))
+      .catch(err => console.log("Service Worker Failed", err));
+  });
+}
 
 
 
