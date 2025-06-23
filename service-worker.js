0a25a85b6d287724d72008efbfaec95c385267b8
Nathaniel McGregor <nathanielmcgr@gmail.com>
2025-06-23 14:20:46 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("app-cache").then((cache) => {
-      return cache.addAll(["/", '/index.html', '/styles.css', '/script.js', '/icons/awp-logo-192x192.png', '/icons/awp-logo-512x512.png']);
-    })
-  );
-});
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open("app-cache").then((cache) => {
+//       return cache.addAll(["/", '/index.html', '/styles.css', '/script.js', '/icons/awp-logo-192x192.png', '/icons/awp-logo-512x512.png']);
+//     })
+//   );
+// });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
